429f00e4c4c78f9090c43d86910813c64598156a
anamarija59 <44981438+anamarija59@users.noreply.github.com>
2018-11-20 22:05:08 +0100

    ana

---
+++ aursanic/newEmptyPHPWebPage.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        echo "<br>";
+        echo "evo mene iz PHP-a!";
+        // ovo je komentar
+        // ovo je isto
+
+        /*
+         * dfkk
+         *khekdj
+         */
+        # ovo je isto linijki komentar
+        /**
+         * ovo je php doc
+         * @author John Doe <john.doe@example.com>
+         * @category programiranje
+         * @license http://URL name
+         * @return int temperatura 
+         
+      
+         
+         */
+        ?>
+    </body>
+</html>
