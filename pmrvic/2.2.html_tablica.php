54fe049f3627ae783e55484504e487bd12ddf6d5
pmrvic <mrvic.predrag@gmail.com>
2018-11-20 21:05:18 +0100

    primjeri htmla

---
+++ pmrvic/2.2.html_tablica.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>TODO supply a title</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  </head>
+  <body>
+    <div>
+
+      <table border="1">
+        <tr>
+          <th>Pon</th>
+          <th>Uto</th>
+          <th>Sri</th>
+          <th>Cet</th>
+          <th>Pet</th>
+          
+        </tr>
+        <tr><td>Banana</td><td>Jabuka</td><td>Kivi</td><td colspan="2">Banana</td>        
+        </tr>
+        <tr><td>Naranča</td><td colspan="3">Šljiva</td><td>Naranča</td></tr>
+        
+      </table>
+      <img src="dobra-kamera.jpg">
+
+
+    </div>
+  </body>
+</html>
+<?php
+
