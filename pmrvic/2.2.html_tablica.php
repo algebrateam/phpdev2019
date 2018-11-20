6714db43bebd3b3c956f8601b7b1889ba672b6ab
pmrvic <mrvic.predrag@gmail.com>
2018-11-20 21:51:50 +0100

    prvi php i html tablica

--- pmrvic/2.2.html_tablica.php
+++ pmrvic/2.2.html_tablica.php
@@ -6,24 +6,27 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
   </head>
   <body>
-    <div>
+<div>
 
-      <table border="1">
-        <tr>
-          <th>Pon</th>
-          <th>Uto</th>
-          <th>Sri</th>
-          <th>Cet</th>
-          <th>Pet</th>
-          
-        </tr>
-        <tr><td>Banana</td><td>Jabuka</td><td>Kivi</td><td colspan="2">Banana</td>        
-        </tr>
-        <tr><td>Naranča</td><td colspan="3">Šljiva</td><td>Naranča</td></tr>
-        
+  <table border="1">
+    <tr>
+      <th>Pon</th>
+      <th>Uto</th>
+      <th>Sri</th>
+      <th>Cet</th>
+      <th>Pet</th>
+
+    </tr>
+    <tr><td>Banana</td><td>Jabuka</td><td>Kivi</td><td colspan="2">Banana</td>
+    </tr>
+    <tr><td>Naranča</td><td colspan="3">Šljiva</td><td>Naranča</td></tr>
+
       </table>
-      <img src="dobra-kamera.jpg">
-
+      <a href="dobra-kamera.jpg">Link na kameru</a><br>
+      <img src="dobra-kamera.jpg" width="150px" height="50px"><br>
+      <img src="dobra-kamera.jpg" width="150px"><br>
+      <img src="dobra-kamera.jpg" height="189px"><br>
+      <img src="dobra-kamera.jpg" width="40%"><br>
 
     </div>
   </body>
