b3edecaa3fb02687d80c4d66926617b712d2ea88
dlezaic <lezaicdrazen99@gmail.com>
2018-11-20 21:54:09 +0100

    html tablica

---
+++ dlezaic/HTMsL.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <title>TODO supply a title</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+        <div>
+            <a href="https://www.w3schools.com/html/default.asp">
+                w3school tutorial
+            </a>
+            
+            <ol>
+                <li>prvi</li>
+                <li>drugi</li>
+                <li>treci</li>
+                <li>cetvrti</li>
+            </ol>
+            <hr>
+            <ul>
+                Crni caj
+                <br>Zeleni caj<br>
+                  
+                
+            </ul>
+        
+        
+        
+        
+        </div>
+    </body>
+</html>
---
+++ dlezaic/prviphp.php
@@ -0,0 +1,20 @@
+<?php
+
+echo "Moji kontakt podaci:<br>";
+include './kontakt.txt';
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+//ovo je komentar
+//ovo je isto
+
+
+/*
+ * 
+ * 
+ * 
+ */
+
