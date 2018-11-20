3df7a5ec57a4f73c928af06d104d2ad0ba1ad25c
mmartinovic96 <blackymateo@gmail.com>
2018-11-20 21:53:14 +0100

    prvi php i tablica

---
+++ mmartinovic/2.1html_primjer.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <title>stranica</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+        <div>
+            <a href="https://www.w3schools.com/html/html_exercises.asp"> w2school</a>
+            <ol>
+                <li>prvi</li>
+                <li>drugi</li>
+                <li>treci</li>
+                <li>cetvrti</li>
+                <li>peti</li>
+                
+            </ol> <br>
+            <hr>
+        </div>
+    </body>
+</html>
---
+++ mmartinovic/newhtml.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <title>TODO supply a title</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+        <div>
+            ovo je iz htmla <br>
+            
+            
+            
+            
+            <?php
+            echo "<br>";
+            echo "Ev mene iz hp-a";
+            //komentar
+            //to je
+            
+            /*
+             * moguce
+             * pa da
+           
+             */
+            # ovo je linijiski
+            
+            /**
+             * php doc
+             * @author John Doe <john.doe@example.com>
+             * @category programiranje
+             * @license http://URL name
+             * @return int temperatura u c˙ Description
+             * 
+             */
+            echo "dodani dokument:<br>";
+            
+            include "neki file";
+            echo "<br>"
+            
+            
+            
+            ?>
+        </div>
+    </body>
+</html>
