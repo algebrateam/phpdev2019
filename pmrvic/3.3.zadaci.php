bae222916434c15eac2dcab3d408b7bc2f6f4d3b
pmrvic <mrvic.predrag@gmail.com>
2018-11-22 21:56:56 +0100

    usporedba operatori

--- pmrvic/3.3.zadaci.php
+++ pmrvic/3.3.zadaci.php
@@ -1,8 +1,33 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+// 3.3.1 zadaci
+// ispisi par brojeva
 
+$a=5;
+$b=67.9;
+
+echo $a." ".$b;
+
+// 3.3.2
+// definiraj a i b varijable i zamijeni im vrijednost
+$a=3;
+$b=5;
+
+Echo "<br>varijabla a=".$a;
+Echo "<br>varijabla b=".$b;
+
+$pom=$b;
+$b=$a;
+$a=$pom;
+
+Echo "<br>varijabla a=".$a;
+Echo "<br>varijabla b=".$b;
+
+
+$imeprez='George Washington';
+echo $imeprez;
+
+$polje=['abc',45,67.99,true, null, false];
+print_r($polje);
+echo "<br>";
+var_dump($polje);
