c5061de701e1cd35bdd2ae0fd5231cdd696b28a5
marko2107 <marko.drnasin@gmail.com>
2018-11-24 21:19:52 +0100

    zadaca

---
+++ mdrnasin/4.3.Zadaci_za_ponavljanje.php
@@ -0,0 +1,56 @@
+<?php
+
+echo "1. Zadatak - %";
+
+echo "<hr>";
+
+$ime = "Marko";
+$prezime = "Drnasin";
+echo "2. Zadatak - ";
+echo $ime . " " . $prezime;
+
+echo "<hr>";
+
+$a = 5;
+echo "3. Zadatak - ";
+echo ++$a;
+echo ', ';
+echo --$a;
+
+echo "<hr>";
+
+$a = (float) $a;
+echo "4. Zadatak - ";
+echo $a;
+
+echo "<hr>";
+
+$a = $a * 5;
+echo "5. Zadatak - ";
+echo $a;
+
+echo "<hr>";
+
+$a = 7;
+$b = 3;
+
+echo "6. Zadatak - ";
+echo $a + $b;
+echo ', ';
+echo $a - $b;
+echo ', ';
+echo $a * $b;
+echo ', ';
+echo round($a / $b, 6);
+echo ', ';
+echo $a % $b;
+
+echo "<hr>";
+
+$a = 4;
+$b = 2;
+echo "7. Zadatak - ";
+echo $a;
+echo ", ";
+echo $b;
+?>
