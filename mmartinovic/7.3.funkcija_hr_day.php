179f32e4a5477797321725ea4e4f3a40e7d2aed6
mmartinovic96 <blackymateo@gmail.com>
2018-12-04 21:57:43 +0100

    funkcija hr_day

---
+++ mmartinovic/7.3.funkcija_hr_day.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * ne prima nikakav parametar,vraca hr naziv danasnjeg dana u tjednu
+ * npr: ponedjeljak,utorak...
+ * @return string dan u tjednu
+ */
+function hr_day(): string {
+    switch (date('w')) {
+        case 0:return 'nedjelja';
+            break;
+        case 1:return 'ponedjeljak';
+            break;
+        case 2:return 'utorak';
+            break;
+        case 3:return 'sri';
+            break;
+        case 4:return 'cet';
+            break;
+        case 5:return 'pet';
+            break;
+
+
+
+
+          
+
+        default:return 'subota';
+            break;
+    }
+    //return date('w'); // w day of week (0 on sunday)
+}
+
+echo hr_day();
