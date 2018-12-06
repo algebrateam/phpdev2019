71672fad86252f15ecbd3c8996a486db2266b877
marko2107 <marko.drnasin@gmail.com>
2018-12-06 21:17:30 +0100

    citanje u datoteci

---
+++ mdrnasin/8.2.0.citanje_datoteke.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Čitanje iz datoteke fopen() 
+ */
+
+$filename='kontakt.txt';
+
+$handle= fopen($filename, 'r');
+
+var_dump($handle);
+echo '<br>';
+$contents= fread($handle, 11);
+var_dump($contents);
+echo '<br>';
+$contents= fread($handle, 11);
+var_dump($contents);
+echo '<br>';
+echo 'trenutno se nalazim na '. ftell($handle).' znaku u datoteci';
