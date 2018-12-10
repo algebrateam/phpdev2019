5d692ee41652b92efcacede55ff5f5a7f618cef7
marko2107 <marko.drnasin@gmail.com>
2018-12-10 13:01:39 +0100

    zadatak 2

--- mdrnasin/8.5.zadaci_za_ponavljanje.php
+++ mdrnasin/8.5.zadaci_za_ponavljanje.php
@@ -3,11 +3,10 @@
 /**
  *
  */
-
 echo 'Zadatak 1. - Program koji čita sadržaj neke txt datoteke: ';
-$filename='kontakt2.txt';
-$mode='r';
-$handle= fopen($filename, $mode);
+$filename = 'kontakt2.txt';
+$mode = 'r';
+$handle = fopen($filename, $mode);
 
 $contents = fread($handle, 45);
 var_dump($contents);
@@ -16,4 +15,13 @@ fclose($handle);
 
 echo 'Zadatak 2. - Obrazac koji zapisuje podatak u .txt file: ';
 
+echo '<form method="POST">'
+ . 'Text: <input name="text"/><br>'
+ . '<input type="submit" name="Pošalji" value="Pošalji"/><br>';
 
+if(isset($_POST["Pošalji"])){
+    $filename='text.txt';
+    $handle= fopen($filename, 'a+');
+    fwrite($handle, $_POST["text"]);
+    fclose($handle);
+}
