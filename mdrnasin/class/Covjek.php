dbd807c7be5fdcd1111467b5f8aa2796d09fcb49
marko2107 <marko.drnasin@gmail.com>
2018-12-12 13:07:20 +0100

    start world

--- mdrnasin/class/Covjek.php
+++ mdrnasin/class/Covjek.php
@@ -8,18 +8,34 @@ namespace World;
  * @author marko
  */
 class Covjek {
-    public $ime="Marko";
-    public $brojOI;
-    private $spol;
-    public $trenutna_brzina=0;
+  public $ime='Marko';
+  public $broj;
+  private $spol='M';
+  public $trenutna_brzina=0;
+  
+  public function ime(){
+    echo 'Moje ime je '. $this->ime.'<br>';
+  }
+  
+  public function get_spol(){
+    if($this->spol=='M'){
+      return 'muško';
+    }
+    else{
+      return 'žensko';
+    }
     
-    public function ime(){
-        echo 'Moje ime je '.$this;
+  }
+  public function set_spol($s){
+    $this->spol=$s;
+  }
+  public function hodaj(){}
+  public function trci(){}
+  public function stani(){}
+  public function komuniciraj(Covjek $cov){
+    $cov->trenutna_brzina+=2;
+    if($this->spol===$cov->spol){
+      $this->trenutna_brzina=100;
     }
-    public function hodaj(){}
-    public function trci(){}
-    public function stani(){}
-    public function komuniciraj(Covijek $cov){
-        $cov->trenutna_brzina+=2;
-    }
+  }  
 }
