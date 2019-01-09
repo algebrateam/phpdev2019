10e7a38eb703fe1069a3ba0fe55735d0a3cb48f2
pmrvic <mrvic.predrag@gmail.com>
2019-01-09 21:12:57 +0100

    forma get

--- pmrvic/class/Linija.php
+++ pmrvic/class/Linija.php
@@ -1,32 +1,36 @@
 <?php
+
 include_once './Tocka.php';
 include_once './ILinija.php';
+
 /**
  * Description of Linija
  *
  * @author pmrvic
  */
-class Linija implements ILinija{
+class Linija implements ILinija {
+
   private $t1;
   private $t2;
-  public function __construct(Tocka $t1, Tocka $t2) { $this->t1=$t1;
-  $this->t2=$t2;
-  
-    
+
+  public function __construct(Tocka $t1, Tocka $t2) {
+    $this->t1 = $t1;
+    $this->t2 = $t2;
   }
+
   public function __toString() {
-   return '<br>Linija '
-    .$this->t1->getkoords()
-     .'----'
-     .$this->t2->getkoords()
-     .' duljina:'
-     .$this->get_length();
+    return '<br>Linija '
+      . $this->t1->getkoords()
+      . '----'
+      . $this->t2->getkoords()
+      . ' duljina:'
+      . $this->get_length();
   }
 
   public function get_length(): float {
-    $raz_x=$this->t2->get_x()-$this->t1->get_x();
-    $raz_y=$this->t2->get_y()-$this->t1->get_y();
-    return sqrt($raz_x*$raz_x+pow($raz_y, 2));
+    $raz_x = $this->t2->get_x() - $this->t1->get_x();
+    $raz_y = $this->t2->get_y() - $this->t1->get_y();
+    return sqrt($raz_x * $raz_x + pow($raz_y, 2));
   }
 
   public function trans_x(int $x) {
@@ -38,7 +42,7 @@ class Linija implements ILinija{
 
   public function trans_y(int $y) {
     $this->t1->move_y($y);
-    $this->t2->move_y($y);   
+    $this->t2->move_y($y);
   }
 
 }
