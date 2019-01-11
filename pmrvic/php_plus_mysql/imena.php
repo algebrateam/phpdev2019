09253528c7a19973c12b22cd2512fca8325c070b
pmrvic <mrvic.predrag@gmail.com>
2019-01-11 22:07:21 +0100

    upload obrazac imena

---
+++ pmrvic/php_plus_mysql/imena.php
@@ -0,0 +1,133 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>TODO supply a title</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <style type="text/css">
+.form-style-3{
+	max-width: 450px;
+	font-family: "Lucida Sans Unicode", "Lucida Grande", sans-serif;
+}
+.form-style-3 label{
+	display:block;
+	margin-bottom: 10px;
+}
+.form-style-3 label > span{
+	float: left;
+	width: 100px;
+	color: #F072A9;
+	font-weight: bold;
+	font-size: 13px;
+	text-shadow: 1px 1px 1px #fff;
+}
+.form-style-3 fieldset{
+	border-radius: 10px;
+	-webkit-border-radius: 10px;
+	-moz-border-radius: 10px;
+	margin: 0px 0px 10px 0px;
+	border: 1px solid #FFD2D2;
+	padding: 20px;
+	background: #FFF4F4;
+	box-shadow: inset 0px 0px 15px #FFE5E5;
+	-moz-box-shadow: inset 0px 0px 15px #FFE5E5;
+	-webkit-box-shadow: inset 0px 0px 15px #FFE5E5;
+}
+.form-style-3 fieldset legend{
+	color: #FFA0C9;
+	border-top: 1px solid #FFD2D2;
+	border-left: 1px solid #FFD2D2;
+	border-right: 1px solid #FFD2D2;
+	border-radius: 5px 5px 0px 0px;
+	-webkit-border-radius: 5px 5px 0px 0px;
+	-moz-border-radius: 5px 5px 0px 0px;
+	background: #FFF4F4;
+	padding: 0px 8px 3px 8px;
+	box-shadow: -0px -1px 2px #F1F1F1;
+	-moz-box-shadow:-0px -1px 2px #F1F1F1;
+	-webkit-box-shadow:-0px -1px 2px #F1F1F1;
+	font-weight: normal;
+	font-size: 12px;
+}
+.form-style-3 textarea{
+	width:250px;
+	height:100px;
+}
+.form-style-3 input[type=text],
+.form-style-3 input[type=date],
+.form-style-3 input[type=datetime],
+.form-style-3 input[type=number],
+.form-style-3 input[type=search],
+.form-style-3 input[type=time],
+.form-style-3 input[type=url],
+.form-style-3 input[type=email],
+.form-style-3 select, 
+.form-style-3 textarea{
+	border-radius: 3px;
+	-webkit-border-radius: 3px;
+	-moz-border-radius: 3px;
+	border: 1px solid #FFC2DC;
+	outline: none;
+	color: #F072A9;
+	padding: 5px 8px 5px 8px;
+	box-shadow: inset 1px 1px 4px #FFD5E7;
+	-moz-box-shadow: inset 1px 1px 4px #FFD5E7;
+	-webkit-box-shadow: inset 1px 1px 4px #FFD5E7;
+	background: #FFEFF6;
+	width:50%;
+}
+.form-style-3  input[type=submit],
+.form-style-3  input[type=button]{
+	background: #EB3B88;
+	border: 1px solid #C94A81;
+	padding: 5px 15px 5px 15px;
+	color: #FFCBE2;
+	box-shadow: inset -1px -1px 3px #FF62A7;
+	-moz-box-shadow: inset -1px -1px 3px #FF62A7;
+	-webkit-box-shadow: inset -1px -1px 3px #FF62A7;
+	border-radius: 3px;
+	border-radius: 3px;
+	-webkit-border-radius: 3px;
+	-moz-border-radius: 3px;	
+	font-weight: bold;
+}
+.required{
+	color:red;
+	font-weight:normal;
+}
+</style>
+  </head>
+  <body>
+        <div style="width: 350px">
+      <form method="GET" action="#">
+        Ime: <input name="broj_imena" type="number" required="true"><br>
+        <input type="submit" name="broj_imena_sbm" value="submit">
+      </form>
+    
+    </div>
+    
+    <div style="width: 350px">
+      <form method="POST" action="#">
+        <?php 
+        if(isset($_GET['broj_imena_sbm'])){ 
+          for($i=1;$i<=$_GET['broj_imena'];$i++){ 
+        ?>
+        <fieldset>       
+
+        Ime: <input name="ime_<?=$i ?>" type="text"><br>
+        Prezime: <input name="prezime_<?=$i ?>" type="text"><br>
+         </fieldset>
+        <?php } ?>
+        <input type="submit" name="imena_sbm" value="submit">
+        <?php } ?>
+        
+      </form>
+    
+    </div>
+  </body>
+</html>
+<?php
+if(isset($_POST['imena_sbm'])){
+  var_dump($_POST);
+}
--- pmrvic/php_plus_mysql/primjer_upload.php
+++ pmrvic/php_plus_mysql/primjer_upload.php
@@ -8,7 +8,7 @@
   <body>
     <div>
       <form method="POST" name="upload" action="#" enctype="multipart/form-data">
-        <input type="file" name="datoteka">
+        <input type="file" name="datoteka" accept="image/png, image/jpeg">
         <input type="submit" name="uploadaj">
         
       </form>
@@ -24,13 +24,28 @@ if(isset($_POST['uploadaj'])){
   
   $upload_dir=__DIR__.'\upload';
   $upload_file= basename($_FILES['datoteka']['name']);
+  $varexp= explode('.', $upload_file);
+  print_r($varexp);
+ echo "<br>ekstenzija je:".end($varexp);
+  array_pop($varexp);
+echo "<br>novi array<br>";
+print_r($varexp);
+echo "<br>Ime filea je<br>". implode(' ',$varexp);
+echo "<hr>";
   
   echo $upload_dir.'<br>';
   echo $upload_file.'<br>';
   
-  if(move_uploaded_file($_FILES['datoteka']['tmp_name'], $upload_dir.'/'.$upload_file)){
+  if(move_uploaded_file(
+    $_FILES['datoteka']['tmp_name'],
+    $upload_dir.'/'.$upload_file)){
     echo 'file je uploadan: ';
     echo '<a href="upload/'.$upload_file.'">file</a>';
+    //var_dump(mime_content_type ( $upload_dir.'/'.$upload_file));
+    if(mime_content_type ( $upload_dir.'/'.$upload_file )=='image/jpeg'){
+      echo '<img src="upload/'.$upload_file.'">';
+      
+    }
   }
   var_dump($_FILES);
 }
