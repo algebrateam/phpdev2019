d10c69d481a21c4054724715b2d903516b2f685c
marko2107 <marko.drnasin@gmail.com>
2019-01-21 21:39:22 +0100

    sqli transakcije

--- mdrnasin/SQL_PHP/mysqli_proc_proba.php
+++ mdrnasin/SQL_PHP/mysqli_proc_proba.php
@@ -15,10 +15,42 @@ if($result=mysqli_query($link, $query)){
     echo mysqli_error($link);
 }
 echo '<hr>';
+
+
 // FETCH ROW
 if($result=mysqli_query($link, $query)){
     while ($row = mysqli_fetch_row($result)) {
         echo $row[0].' '.$row[1].' '.$row[2];
+    }
+    echo '<br>';
+    echo 'Broj redova: '. mysqli_num_rows($result);
+}else{
+    echo 'Upit nije izvršen';
+    echo mysqli_error($link);
+}
+
+echo '<hr>';
+
+
+// FETCH ARRAY
+if($result=mysqli_query($link, $query)){
+    while ($row = mysqli_fetch_array($result)) {
+        echo $row[0].' '.$row['imeStud'].' '.$row[2];
+    }
+    echo '<br>';
+    echo 'Broj redova: '. mysqli_num_rows($result);
+}else{
+    echo 'Upit nije izvršen';
+    echo mysqli_error($link);
+}
+
+echo '<hr>';
+
+
+// FETCH OBJECT
+if($result=mysqli_query($link, $query)){
+    while ($row = mysqli_fetch_object($result)) {
+        echo $row->mbrStud.' '.$row->imeStud.' '.$row->prezStud;
     }
     echo '<br>';
     echo 'Broj redova: '. mysqli_num_rows($result);
